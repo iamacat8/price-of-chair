9e9c7f97f60fbcb4a35989f81aa56d97a069a60e
iamacat8 <34300931+iamacat8@users.noreply.github.com>
2017-12-10 00:52:45 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
 from src.common.database import Database
 
-__author__ = 'jslvtr'
-
-
 app = Flask(__name__)
 app.config.from_object('src.config')
 app.secret_key = "123"
